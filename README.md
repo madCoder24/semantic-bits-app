b1b4087f751d38af37ea36af7bffc214d1ff5144
Ryan <r.bauer234@gmail.com>
2017-04-19 20:23:04 -0700

    end to end testing and readme updates

--- README.md
+++ README.md
@@ -12,5 +12,17 @@ ng build
 
 node server.js
 
+# Quality Checks
+
+This project has tslint enabled and can be ran with the command "ng lint". This will display any linting issues with the project and the associated TS files. 
+
+# End to End tests
+
+A couple end to end tests have been defined and they can be run by first starting the local server by running "ng serve". Then you can run "ng e2e" to run the end to end tests.
+
+# Unit tests
+
+Unit tests can be ran with the command "ng test". 
+
 # MongoDB Download and Install Instructions
 https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,18 @@
 import { SemanticAppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('semantic-app App', function() {
-  let page: SemanticAppPage;
 
   beforeEach(() => {
-    page = new SemanticAppPage();
+    browser.get('');
+    browser.waitForAngular();
   });
 
   it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(element(by.css('app-root h1')).getText()).toEqual('Adverse Events');
+  });
+
+  it('should have EventsComponent defined', () => {
+    expect(element(by.css('app-events')).isPresent()).toBeTruthy();
   });
 });
